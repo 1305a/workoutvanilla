6d1d6afdc04f06480f15417eb5193d8d33b3a1ac
1305a <1305andrew2@gmail.com>
2021-04-13 19:09:19 +0300

    19:08 13 04 2021

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,19 @@
+const quantity = document.querySelector('.input-quantity')
+const taskArray = ['Отжимание','Планка (мин)','Скакалка','Бицепс','Трицепс','Приседания','Колесо','Пресс','Эспандер','По желанию']
 
+function formingObjects(quantityNumber) {
+    for (let index = 0; index < quantityNumber; index++) {
+        console.log(quantityNumber)
+        const altName = `Игрок ${index+1}`
+        const player = {'name':'','altName':altName}
+        taskArray.forEach(element => {
+            player[element] = 0
+        })
+    }
+}
+
+quantity.addEventListener ('change', (event) => {
+    formingObjects(quantity.valueAsNumber)
+})
+
+formingObjects(quantity.valueAsNumber)
