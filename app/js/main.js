fee007ecab85c5e0045a2e5ffe8081ea6110b416
1305a <1305andrew2@gmail.com>
2021-04-14 00:10:47 +0300

    00:10 14 04 2021 home

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,24 @@
 const quantity = document.querySelector('.input-quantity')
 const taskArray = ['Отжимание','Планка (мин)','Скакалка','Бицепс','Трицепс','Приседания','Колесо','Пресс','Эспандер','По желанию']
+const playerArray = []
 
 function formingObjects(quantityNumber) {
+    playerArray.length = 0
     for (let index = 0; index < quantityNumber; index++) {
-        console.log(quantityNumber)
         const altName = `Игрок ${index+1}`
         const player = {'name':'','altName':altName}
         taskArray.forEach(element => {
             player[element] = 0
         })
-        console.log(player)
+        playerArray.push(player)
     }
+    console.dir(playerArray)
 }
+
+function formingTable() {
+
+}
+
 
 quantity.addEventListener ('change', (event) => {
     formingObjects(quantity.valueAsNumber)
