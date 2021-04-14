8403dcd73909fc331c86ac9939136d6ae196b417
1305a <1305andrew2@gmail.com>
2021-04-14 13:55:48 +0300

    13:55 14 04 2021

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,10 @@
 const quantity = document.querySelector('.input-quantity')
+const taskTable = document.querySelector('.table')
 const taskArray = ['Отжимание','Планка (мин)','Скакалка','Бицепс','Трицепс','Приседания','Колесо','Пресс','Эспандер','По желанию']
 const playerArray = []
 
-function formingObjects(quantityNumber) {
+
+function objectForming(quantityNumber) {
     playerArray.length = 0
     for (let index = 0; index < quantityNumber; index++) {
         const altName = `Игрок ${index+1}`
@@ -12,16 +14,48 @@ function formingObjects(quantityNumber) {
         })
         playerArray.push(player)
     }
-    console.dir(playerArray)
+    //console.log(playerArray)
+    tableForming(taskArray,playerArray)
 }
 
-function formingTable() {
+function tableForming(taskArr,playerArr) {
+    while (taskTable.firstChild) {
+        taskTable.removeChild(taskTable.firstChild);
+    }
+        
+    let divColumnTask = document.createElement('div')
+    divColumnTask.className = 'column-task'
+    taskTable.appendChild(divColumnTask)
+    for (let i = 0; i < taskArr.length; i++) {
+        let div = document.createElement('div')
+        div.className = 'row-task'
+        div.innerHTML = taskArr[i]
+        divColumnTask.appendChild(div)
+    }
+
+    for (let j = 0; j < playerArr.length; j++) {
+        let divColumnPlayer = document.createElement('div')
+        divColumnPlayer.className = 'column-task'
+        taskTable.appendChild(divColumnPlayer)
+        for (let k = 0; k < taskArr.length; k++) {
+            console.log(playerArr[j][taskArr[k]])
+            let divRow = document.createElement('div')
+            divRow.className = 'row-player'
+            divColumnPlayer.appendChild(divRow)
+            let input = document.createElement('input')
+            input.value = playerArr[j][taskArr[k]]
+            input.className = 'row-input'
+            
+            divRow.appendChild(input)
+        }
+        
+    }
+
 
 }
-
 
 quantity.addEventListener ('change', (event) => {
-    formingObjects(quantity.valueAsNumber)
+    objectForming(quantity.valueAsNumber)
 })
 
-formingObjects(quantity.valueAsNumber)
+objectForming(quantity.valueAsNumber)
