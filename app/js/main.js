1e966e9aae4bc5473e2ff37ff2124410ee990151
1305a <1305andrew2@gmail.com>
2021-04-16 18:53:39 +0300

    18:53 16 04 2021

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,11 @@ const taskTable = document.querySelector('.table')
 const calculateButton = document.querySelector('.calculate-button')
 const testButton = document.querySelector('.test-button')
 const difText = document.querySelector('.difficulty-text')
+const modalResults = document.querySelector('.modal-wrapper')
+const modal = document.querySelector('.modal')
+const modalOverlay = document.querySelector('.modal-overlay')
+const modalClose = document.querySelector('.modal-close')
+let difficultyLevel = ''
 const taskArray = ['Отжимание','Планка (мин)','Скакалка','Бицепс','Трицепс','Приседания','Колесо','Пресс','Эспандер','По желанию']
 const taskArrayTest = [20,2,40,30,20,30,5,30,20,10]
 const playerArray = []
@@ -89,18 +94,21 @@ function tableForming(taskArr,playerArr) {
         difText.innerHTML = `3 задания / повторений 70-85%`
         difText.classList.remove('difficulty-text-yellow','difficulty-text-red')
         difText.classList.add('difficulty-text-green')
+        difficultyLevel = 'easy'
     }
     else if (difLevel === 'normal') {
         taskNumbers = 5
         difText.innerHTML = `5 заданий / повторений 80-95%`
         difText.classList.remove('difficulty-text-green','difficulty-text-red')
         difText.classList.add('difficulty-text-yellow')
+        difficultyLevel = 'normal'
     }
     else if (difLevel ==='hard') {
         taskNumbers = 7
-        difText.innerHTML = '7 заданий / повторений 90-105% / Успехов!:)'
+        difText.innerHTML = '7 заданий / повторений 90-110% / Успехов!:)'
         difText.classList.remove('difficulty-text-yellow','difficulty-text-green')
         difText.classList.add('difficulty-text-red')
+        difficultyLevel = 'hard'
     }
 }
 
@@ -121,16 +129,55 @@ function calculate() {
     
         for (let ra = 0; ra < taskNumbers; ra++) {
             const taskInput = document.getElementById(playerArray[pl].altName+randomArray[ra])
-            finalArray[pl][randomArray[ra]] = taskInput.valueAsNumber
+            
+            if (difficultyLevel==='easy') {
+                getRandomFloat(0.7, 0.85)
+                finalArray[pl][randomArray[ra]] = Math.floor(getRandomFloat(0.7, 0.85)*taskInput.valueAsNumber)
+            }
+            else if (difficultyLevel==='normal') {
+                getRandomFloat(0.7, 0.85)
+                finalArray[pl][randomArray[ra]] = Math.floor(getRandomFloat(0.8, 0.95)*taskInput.valueAsNumber)
+            }
+
+            else if (difficultyLevel==='hard') {
+                getRandomFloat(0.7, 0.85)
+                finalArray[pl][randomArray[ra]] = Math.floor(getRandomFloat(0.9, 1.10)*taskInput.valueAsNumber)
+            }
         }
     }
     showResults(finalArray)
 }
 
-function showResults(finalArray) {
-    
+function getRandomFloat(min, max) {
+    return Math.random() * (max - min) + min;
 }
 
+function showResults(finalArray) {
+    while (modalResults.firstChild) {
+        modalResults.removeChild(modalResults.firstChild);
+    }
+        
+    for (let i = 0; i < finalArray.length; i++) {
+        let divColumnElement = document.createElement('div')
+        divColumnElement.className = 'modal-element'
+        modalResults.appendChild(divColumnElement)
+        
+        for (let key in finalArray[i]) {
+            let divModalItem = document.createElement('div')
+            
+            if (key==='name') {
+                divModalItem.className = 'modal-item-heading'
+                divModalItem.innerHTML = finalArray[i][key].length > 0 ? finalArray[i][key] : finalArray[i]['altName']
+            }
+            
+            if ((key!=='name')&&(key!=='altName')) {
+                divModalItem.className = 'modal-item'
+                divModalItem.innerHTML = `${key} - ${finalArray[i][key]}`
+            }
+            divColumnElement.appendChild(divModalItem)
+        }
+    }
+}
 
 quantity.addEventListener ('change', (event) => {
     event.preventDefault()
@@ -148,6 +195,22 @@ const difficulty = document.querySelectorAll('input[name="difficulty"]');
 calculateButton.addEventListener ('click', (event) => {
     event.preventDefault()
     calculate()
+    if (modal.classList.contains('modal-show')===false) {
+        modal.classList.add('modal-show')
+    }
+    if (modalOverlay.classList.contains('modal-show')===false) {
+        modalOverlay.classList.add('modal-show')
+    }
+})
+
+modalClose.addEventListener ('click', (event) => {
+    event.preventDefault()
+    if (modal.classList.contains('modal-show')===true) {
+        modal.classList.remove('modal-show')
+    }
+    if (modalOverlay.classList.contains('modal-show')===true) {
+        modalOverlay.classList.remove('modal-show')
+    }
 })
 
 testButton.addEventListener ('click', (event) => {
